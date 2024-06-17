4db78c465f985fdbd6f336a828c40f3b55f50eac
SkyLanYT <skylanyt18062005@gmail.com>
2024-06-17 15:51:46 +0300

    change admin-panel Django

--- myenv/tent_backend/tent_backend/models.py
+++ myenv/tent_backend/tent_backend/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     photo = models.ImageField(upload_to='photos/')  # Фото
@@ -7,6 +8,9 @@ class Post(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)  # Ціна
     location = models.CharField(max_length=100)  # Місце
     created_at = models.DateTimeField(auto_now_add=True)  # Час розміщення оголошення
+    first_name = models.CharField(max_length=100)  # Ім'я замовника
+    last_name = models.CharField(max_length=100)  # Прізвище замовника
+    description = models.TextField()  # Опис оголошення
 
     def __str__(self):
         return self.title
